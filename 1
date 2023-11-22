c9ae5d06235d2f9dbd3632e5f3f7dd0f66ba06fe
root <root@ip-172-31-40-79.ap-south-1.compute.internal>
2023-11-22 06:34:10 +0000

    backend

---
+++ 1
@@ -0,0 +1,6 @@
+FROM node:16.17.1
+WORKDIR /app
+COPY ./ .
+RUN npm install
+CMD ["node","index.js"]
+
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors())
+
+let data = [
+    {
+        id : 1,
+        msg : "1"
+    },
+    {
+        id : 2,
+        msg : "1"
+    },
+    {
+        id : 3,
+        msg : "1"
+    },
+    {
+        id : 4,
+        msg : "1"
+    },
+    {
+        id : 5,
+        msg : "1"
+    },
+]
+
+app.get('/data',(req,res)=>{
+    res.status(200).json({data : data})
+})
+
+app.listen(5000, ()=>{
+    console.log("Port connected")
+})
