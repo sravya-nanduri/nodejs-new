ba5e4a6e6a1790c2cfa1580c7e42979c809d7453
sravya-nanduri <84439938+sravya-nanduri@users.noreply.github.com>
2024-03-28 15:36:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,69 @@
+// const express = require('express');
+// const cors = require('cors');
+
+// const app = express();
+
+// app.use(cors())
+
+// let data = [
+//     {
+//         id : 1,
+//         msg : "1"
+//     },
+//     {
+//         id : 2,
+//         msg : "1"
+//     },
+//     {
+//         id : 3,
+//         msg : "1"
+//     },
+//     {
+//         id : 4,
+//         msg : "1"
+//     },
+//     {
+//         id : 5,
+//         msg : "1"
+//     },
+// ]
+
+// app.get('/data',(req,res)=>{
+//     res.status(200).json({data : data})
+// })
+
+// app.listen(5000, ()=>{
+//     console.log("Port connecteddd")
+// })
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
 
-app.use(cors())
+// Smell: Long method
+app.use(cors());
 
-let data = [
-    {
-        id : 1,
-        msg : "1"
-    },
-    {
-        id : 2,
-        msg : "1"
-    },
-    {
-        id : 3,
-        msg : "1"
-    },
-    {
-        id : 4,
-        msg : "1"
-    },
-    {
-        id : 5,
-        msg : "1"
-    },
-]
+// Smell: Duplicate code
+const data = [
+    { id: 1, msg: "1" },
+    { id: 2, msg: "1" },
+    { id: 3, msg: "1" },
+    { id: 4, msg: "1" },
+    { id: 5, msg: "1" }
+];
 
-app.get('/data',(req,res)=>{
-    res.status(200).json({data : data})
-})
+// Smell: Complexity
+app.get('/data', (req, res) => {
+    if (req.query && req.query.filter) {
+        const filteredData = data.filter(item => item.msg === req.query.filter);
+        res.status(200).json({ data: filteredData });
+    } else {
+        res.status(200).json({ data: data });
+    }
+});
 
-app.listen(5000, ()=>{
+// Smell: Code style violation (Improper indentation)
+app.listen(5000, () => {
     console.log("Port connecteddd")
 })
+
